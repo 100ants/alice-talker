b71202538097959f2bc150a8777153954a805a15
100ants <didyk.ivan.90@mail.ru>
2019-07-04 11:02:40 +0300

    First commit

---
+++ app.py
@@ -0,0 +1,90 @@
+# Импортирует поддержку UTF-8.
+from __future__ import unicode_literals
+
+# Импортируем модули для работы с JSON и логами.
+import json
+import logging
+import eliza
+
+# Импортируем подмодули Flask для запуска веб-сервиса.
+from flask import Flask, request
+app = Flask(__name__)
+
+
+logging.basicConfig(level=logging.DEBUG)
+
+# Хранилище данных о сессиях.
+sessionStorage = {}
+
+# Задаем параметры приложения Flask.
+@app.route("/", methods=['POST'])
+def main():
+    # Функция получает тело запроса и возвращает ответ.
+    logging.info('Request: %r', request.json)
+
+    response = {
+        "version": request.json['version'],
+        "session": request.json['session'],
+        "response": {
+            "end_session": False
+        }
+    }
+
+    handle_dialog(request.json, response)
+
+    logging.info('Response: %r', response)
+
+    return json.dumps(
+        response,
+        ensure_ascii=False,
+        indent=2
+    )
+
+# Функция для непосредственной обработки диалога.
+def handle_dialog(req, res):
+    user_id = req['session']['user_id']
+
+    if req['session']['new']:
+        # Это новый пользователь.
+        # Инициализируем сессию и спросим его о здоровье
+
+        sessionStorage[user_id] = {
+            'suggests': [
+                "Не хочу.",
+                "Не буду.",
+                "Отстань!",
+            ],
+            'elizabot': eliza.eliza()
+        }
+
+        res['response']['text'] = 'Hello.  How are you feeling today?'
+        res['response']['buttons'] = get_suggests(user_id)
+        return
+
+    # Если нет, то продолжим беседу
+    res['response']['text'] = sessionStorage[user_id]['elizabot'].respond(req['request']['original_utterance'])
+
+# Функция возвращает две подсказки для ответа.
+def get_suggests(user_id):
+    session = sessionStorage[user_id]
+
+    # Выбираем две первые подсказки из массива.
+    suggests = [
+        {'title': suggest, 'hide': True}
+        for suggest in session['suggests'][:2]
+    ]
+
+    # Убираем первую подсказку, чтобы подсказки менялись каждый раз.
+    session['suggests'] = session['suggests'][1:]
+    sessionStorage[user_id] = session
+
+    # Если осталась только одна подсказка, предлагаем подсказку
+    # со ссылкой на Яндекс.Маркет.
+    if len(suggests) < 2:
+        suggests.append({
+            "title": "Ладно",
+            "url": "https://market.yandex.ru/search?text=слон",
+            "hide": True
+        })
+
+    return suggests
